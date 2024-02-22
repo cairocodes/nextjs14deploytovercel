6f32019256139dadd46855750ad8f763e9b122f1
Cairocoders <cairocoders@gmail.com>
2024-02-22 20:42:49 +0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2 className={`mb-3 text-2xl font-semibold`}>
-        Next JS Deploy to Vercel
+        Next JS Deploy to Vercel Changes!
       </h2> 
     </main>
   );
